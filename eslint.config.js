99c98f6a208569654cd531b603392b0521ce89ae
Carlos Toro <csantve@gmail.com>
2024-06-13 19:17:42 -0500

    add config files

---
+++ eslint.config.js
@@ -0,0 +1,53 @@
+// @ts-check
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import stylistic from "@stylistic/eslint-plugin";
+import reactRefresh from "eslint-plugin-react-refresh";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRecommended from "eslint-plugin-react/configs/recommended.js";
+import reactJsxRuntime from "eslint-plugin-react/configs/jsx-runtime.js";
+
+// noinspection JSUnusedGlobalSymbols
+export default tseslint.config(
+    eslint.configs.recommended,
+    reactRecommended,
+    reactJsxRuntime,
+    ...tseslint.configs.strictTypeChecked,
+    ...tseslint.configs.stylisticTypeChecked,
+    {
+        languageOptions: {
+            parserOptions: {
+                ecmaVersion: "latest",
+                sourceType: "module",
+                project: ["./tsconfig.json", "./tsconfig.node.json"],
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+        plugins: {
+            "@stylistic": stylistic,
+            "react-hooks": reactHooks,
+            "react-refresh": reactRefresh
+        },
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            "react-refresh/only-export-components": ["warn", { allowConstantExport: true },],
+            "@stylistic/quotes": ["error", "double"],
+            "@stylistic/semi": ["error", "always"],
+            "@stylistic/no-extra-semi": ["error"],
+            "@stylistic/indent": ["error", 4],
+            "@stylistic/no-trailing-spaces": ["error"],
+            "@stylistic/eol-last": ["error", "always"],
+            "@stylistic/max-len": ["error", 120],
+            "eqeqeq": ["error", "smart"],
+            "@typescript-eslint/no-misused-promises": [
+                "error",
+                {
+                    "checksVoidReturn": false
+                }
+            ],
+        }
+    }
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vitejs.dev/config/
+// noinspection JSUnusedGlobalSymbols
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src")
+        }
+    },
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: id => {
+                    if (id.includes("node_modules")) {
+                        return "vendor";
+                    }
+                }
+            }
+        }
+    }
+});
